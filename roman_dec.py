dda5b1e9a4560fc4afa7224a155252deb0dce3fe
KNightFuryX <60267521+KNightFuryX@users.noreply.github.com>
2022-02-12 17:38:37 +0545

    Clean code

--- roman_dec.py
+++ roman_dec.py
@@ -3,7 +3,7 @@ ROMAN_VAL = [
     ('XC', 90), ('L', 50), ('XL', 40), ('X', 10), ('IX', 9), ('V', 5),
     ('IV', 4), ('I', 1)
 ]
-"""MCMXLIX = 1949"""
+
 def solution(roman):
     i = 0
     result = 0
